41f59856be5ebb5b9fa27c0b7ed31a3b9dadb43a
PadroxDev <antoinevolletfr@gmail.com>
2023-04-03 18:26:48 +0200

    Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
+  get "about", to: "about#index", as: :about
+  get "watchlist", to: "watchlist#index", as: :watchlist
+  get "news-calendar", to: "news_calendar#index", as: :news_calendar
+  get "recommendations", to: "recommendations#index", as: :recommendations
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up_pute", to: "registrations#create"
+
   root to: "main#index";
-  
-  get "about", to: "about#index", as: :about;
-  get "watchlist", to: "watchlist#index", as: :watchlist;
-  get "news-calendar", to: "news_calendar#index", as: :news_calendar;
-  get "recommendations", to: "recommendations#index", as: :recommendations;
 end
